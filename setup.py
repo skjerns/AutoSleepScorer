760e072117ce4d90e38f7367e0541ed56f32519e
Simon Kern <simon@momala.org>
2018-01-12 11:17:31 +0100

    testing this thing here

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sleepscorer',
-      version='0.231',
+      version='0.232',
       description='An Automatic Sleep Stage Classification Package',
       url='http://github.com/skjerns/AutoSleepScorer',
       author='skjerns',
