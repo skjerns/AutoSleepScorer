99f769d1c4788eee41ff467a946421f76a2125a4
Simon Kern <14980558+skjerns@users.noreply.github.com>
2021-09-12 15:38:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='sleepscorer',
       author_email='sikern@uos.de',
       license='See Github',
       packages=['sleepscorer'],
-      install_requires=['keras','mne', 'tqdm', 'sklearn', 'matplotlib'],
+      install_requires=['keras==2.0.6', 'tensorflow==1.3.0','h5py==2.7.0', 'mne', 'tqdm', 'matplotlib'],
       zip_safe=False)
